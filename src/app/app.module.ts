6ead1f08df1d5bb3858fbff39bcf98b60a937875
JAPHETHNYARANGA <nyaranga4@gmail.com>
2021-08-28 10:57:21 +0300

    added client id &secret

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProfileComponent } from './components/profile/profile.component';
-import {}
+
 
 @NgModule({
   declarations: [
--- src/app/services/profile.service.ts
+++ src/app/services/profile.service.ts
@@ -7,5 +7,9 @@ import 'rxjs/add/operator/map'
 })
 export class ProfileService {
 
+  private username:string | undefined;
+  private clientid= '62b5d4bdf6b0a3d55bc5';
+  private clientsecret = 'a158489cfcb493419afe97386baefd54822bbd69';
+
   constructor(private http:HttpClient) { }
 }
