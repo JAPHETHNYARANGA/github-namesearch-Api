901ba4a15cf13d49aa08963d3b1f924ac26cd75e
JAPHETHNYARANGA <nyaranga4@gmail.com>
2021-08-28 11:39:29 +0300

    fixed the error by removing the outdated json-file

--- src/app/services/profile.service.ts
+++ src/app/services/profile.service.ts
@@ -7,16 +7,16 @@ import { map } from "rxjs/operators";
 })
 export class ProfileService {
 
-  private username:string;
-  private clientid= '62b5d4bdf6b0a3d55bc5';
+  private username: string;
+  private clientid = '62b5d4bdf6b0a3d55bc5';
   private clientsecret = 'a158489cfcb493419afe97386baefd54822bbd69';
 
-  constructor(private http:HttpClient) { 
+  constructor(private http: HttpClient) {
     console.log("service is now ready");
     this.username = 'JAPHETHNYARANGA'
   }
-  getProfileInfo(){
+  getProfileInfo() {
     return this.http.get("https://api.github.com/users/" + this.username + "?client_id=" + this.clientid + "&client_secret=" + this.clientsecret)
-    .pipe(map(res=>res.json()));
+      ;
   }
 }
