af5f518b4f3be93d15402fbfc027d8e5a9a49522
JAPHETHNYARANGA <nyaranga4@gmail.com>
2021-08-28 12:56:03 +0300

    refactored the profite.ts file

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,20 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { ProfileService } from 'src/app/services/profile.service';
 
+
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
+
+// export interface Config{
+//   followers :number;
+//   company:string;
+// }
+
 export class ProfileComponent implements OnInit {
 
-  constructor(private profileService:ProfileService) {
-    this.profileService.getProfileInfo().subscribe(profile =>{
-      console.log(profile);
-    })
-   }
+ 
+
+  constructor(private profileService:ProfileService) {}
+   collection=<any>{};
 
   ngOnInit(): void {
+    this.profileService.getProfileInfo().subscribe((result)=>{
+      console.warn(result)
+      this.collection=result
+    })
+    
   }
 
 }
